79253cd5019f1d6975175ab01c896117be050e1a
MinGyu Kim <dooek1318@g.yju.ac.kr>
2024-04-29 14:22:02 +0900

    4월 29일 업로드

---
+++ my_project/expression.py
@@ -0,0 +1,9 @@
+# 정수와 부동 소수점 수 연산
+num_int = 10 # 이 식에서 연산자는 =(대입 연산자). 우항의 값을 좌항으로 옮긴다
+num_float = 3.14
+
+# 연산 수행
+result = num_int + num_float # 연산자는 이항연산자 2개(= , +)
+
+print("결과:", result) # 연산자는 1개(,)
+print("결과의 자료형:", type(result))
